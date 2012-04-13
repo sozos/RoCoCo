19133c4e0ce7b333b513f94aed7be2ae0e7dc4e4
Lee Yik Jiun <hyperangel129@hotmail.com>
2012-04-13 22:00:39 +0800

    Copied all the files over

---
+++ STS.js
@@ -0,0 +1,285 @@
+// Import FD.js library
+importScripts('fd.js');
+
+// Event Listener
+addEventListener('message', function(e) {
+	var data = e.data;
+	switch(data.cmd) {
+	case 'generate_first_soln':
+		STS_Engine.update_data(data.arg);
+		STS_Engine.generate_first_soln(); // just generate one solution
+		break;
+	case 'run':
+		STS_Engine.generate_solns(data.arg); // data.arg = number of iterations before stopping
+		break;
+    case 'grab_results':
+		postMessage({'cmd': 'soln',
+					 'arg': STS_Engine.return_soln(data.arg)});
+        break;
+	}
+}, false);
+
+// STS ENGINE
+var STS_Engine = (function() {
+	// The Object "pointer"
+	var that = {};
+	
+	// Internal variables
+	var state = ''; // a string value ('generate_first' | 'running' | 'paused' | 'completed')
+	var spaceStack = []; // To store unexplored spaces
+	var solnArr = []; // To store completed spaces
+	var varnameArray = [];
+
+	// External variables
+	var numTeams = 0; // (from UI)
+	var numVenues = 0; // (from UI)
+	var numRounds = 0; // (Derived from numTeams)
+	var constraintTArr = []; // (from UI)
+	var constraintVArr = [];
+
+	// Calculation functions
+	function initSpace() {
+		var total = numRounds * numTeams + 
+					numRounds * numTeams + 
+					0.5 * numRounds * numTeams * numTeams + 
+					numRounds * numTeams +
+					numRounds * numTeams +
+					numRounds * numTeams;
+		postMessage({'cmd': 'totalProg', 'arg': total});
+		
+		// Wipe everything to a clean slate
+		spaceStack = [];
+		solnArr = [];
+     
+		var r, t; // rounds and teams to be used in for loops
+		var arr = []; // used to make rounds and teams distinct
+		
+		// Create new space
+		var S = new FD.space();
+     
+		// Setting up R_T_O & R_T_V variables
+		// Create the "sub-arrays" in the array and "push" the variables into the array
+		for (r = 1; r <= numRounds; r++) {
+			varnameArray[r] = [];
+			for (t = 1; t <= numTeams; t++) {
+				varnameArray[r][t] = 'R' + r + 'T' + t;
+			}
+		}
+     
+		// Declaring each variable into S
+		for (r = 1; r <= numRounds; r++) {
+			for (t = 1; t <= numTeams; t++) {
+				S.decl(varnameArray[r][t]+'O', [[1 - (numTeams % 2), numTeams]]); // 0 for "bye"
+				S.decl(varnameArray[r][t]+'V', [[0, numVenues]]); // 0 for "bye" venues
+			}
+		}
+
+		// Adding initial constraints
+		for (r = 1; r <= numRounds; r++) {
+			arr = [];
+				for (t = 1; t <= numTeams; t++) {
+				//Teams cannot play themselves
+				S.neq(varnameArray[r][t]+'O', S.const(t));
+
+				//Team i vs Team j iff Team j vs Team i
+				for (var o = t + 1; o <= numTeams; o++) {
+					var tempVar = S.temp([[0, 1]]);
+					S.reified('eq', [varnameArray[r][t]+'O', S.const(o)], tempVar); // Team plays opponent
+					S.reified('eq', [varnameArray[r][o]+'O', S.const(t)], tempVar); // Opponent plays team
+					S.reified('eq', [varnameArray[r][t]+'V', varnameArray[r][o]+'V'], tempVar); // Team and opponents play at same venue
+           
+					var tempVar2 = S.temp([[0, 1]]); // When you get a "bye", your venue is "0"
+					S.reified('eq', [varnameArray[r][t]+'O', S.const(0)], tempVar2);
+					S.reified('eq', [varnameArray[r][t]+'V', S.const(0)], tempVar2);
+       
+					var tempVar3 = S.temp([[0, 1]]); // When you don't get a "bye", your venue is 1~numVenues
+					S.reified('neq', [varnameArray[r][t]+'O', S.const(0)], tempVar3);
+					S.reified('neq', [varnameArray[r][t]+'V', S.const(0)], tempVar3);
+				}
+				arr.push(varnameArray[r][t]+'O');
+			}
+			//Each round, teams are distinct
+			S.distinct(arr);
+		}
+     
+		for (t = 1; t <= numTeams; t++) {
+			arr = [];
+			for (r = 1; r <= numRounds; r++) {
+				arr.push(varnameArray[r][t]+'O');
+			}
+			//Each team play against distinct teams
+			S.distinct(arr);
+		}
+
+		// constraint team array
+		for (var varname in constraintTArr) {
+			for (var index in constraintTArr[varname]) {
+				S.neq(varname, S.const(constraintTArr[varname][index]));
+			}
+		}
+		
+		// constraint venue array
+		for (var varname in constraintVArr) {
+			for (var index in constraintVArr[varname]) {
+				S.neq(varname, S.const(constraintVArr[varname][index]));
+			}
+		}
+
+		try {
+			S.propagate(); // Potential point of failure
+			
+			// Add space to spaceStack
+			spaceStack.push(S);
+
+			// Run soln_generator
+			soln_generator(1);
+		} catch (e) {
+			// Do nothing so that solnArr is empty when requested by UI
+			// alert('Constraints too strict');
+			postMessage({'cmd': 'partial', 'arg': STS_Engine.return_solnArrLen()});
+		}
+	}
+	
+	function soln_generator(num_times) {
+		for (var i = 0; i < num_times; i++) {
+			if (spaceStack.length === 0) { // No more unexplored spaces
+				postMessage({'cmd': 'completed', 'arg': STS_Engine.return_solnArrLen()}); // To tell UI generator is done
+				break;
+			} else {
+				naive_brancher();
+			}
+			if (solnArr.length === 0) {
+				i--;
+			}
+		}
+		postMessage({'cmd': 'partial', 'arg': STS_Engine.return_solnArrLen()}); // To tell UI its partially done generating
+	}
+	
+	function naive_brancher() {
+		var curSpace = spaceStack.pop(); // soln_generator already guarantees that spaceStack is not empty
+		var varName;
+		var newSpace;
+		var i;
+     
+		if (curSpace.is_solved()) {
+			// Weed out spaces where non-zero venue values are not 0 or 2
+			if (zeroortwo(curSpace)) {
+				solnArr.push(curSpace);
+			} else {} // Nothing to do here.
+		} else {
+			varName = firstNonConstVar(curSpace);
+			for (i = curSpace.vars[varName].min(); i <= curSpace.vars[varName].max(); i++) {
+				try {
+					newSpace = curSpace.clone();
+					newSpace.eq(varName, newSpace.const(i));
+					newSpace.propagate();
+					spaceStack.push(newSpace);
+				} catch(e) {};
+			}
+		}
+	}
+
+	// Interaction functions
+	that.set_state_to_running = function() {
+		state = 'running';
+	};
+	
+	that.set_state_to_paused = function() {
+		state = 'paused';
+	};
+	
+	that.update_data = function(UI_Obj) {
+		// Grab data
+		numTeams = UI_Obj.numTeams;
+		numVenues = UI_Obj.numVenues;
+		constraintTArr = UI_Obj.constraintTArr;
+		constraintVArr = UI_Obj.constraintVArr;
+		
+		// Update numRounds
+		numRounds = numTeams % 2 === 0 ? numTeams - 1: numTeams;
+		// If even numTeams, 1 less round than numTeams
+		// If odd numTeams, then we have same number (due to 1 round where you play no one - "bye")
+		// This works too: numRounds = numTeams - (1 - numTeams%2);
+	};
+	
+	that.generate_first_soln = function() {
+		initSpace();
+		//soln_generator(1);
+	};
+	
+	that.generate_solns = function(num) {
+		soln_generator(num);
+	};
+	
+	that.return_soln = function(index) {
+		return parseSoln(index);
+	};
+	
+	that.return_state = function() {
+		return state;
+	};
+	
+	that.return_solnArrLen = function() {
+		return solnArr.length;
+	}
+
+	// Helper functions
+	function firstNonConstVar(Space) {
+	    for (var varName in Space.vars) {
+			if (Space.vars[varName].size() !== 1) {
+				return varName;
+			}
+		}
+		throw "Space is solved."; // Shouldn't reach here
+	}
+	
+	function zeroortwo(Space) {
+		var i,r;
+		//var numVenues = venuenameArray.length;
+		// Initialise vArr to all 0
+		var vArr = new Array(numVenues+1); // Additional "slot" for venue 0
+		for (i = 0; i < vArr.length; i++) {
+			vArr[i] = [];
+			for (r = 1; r <= numRounds; r++) {
+				vArr[i][r] = 0;
+			}
+		}
+
+		// Populating vArr with the number of time each venue is used
+		for (i in Space.vars) {
+			if (i[0] === 'R' && i[2] === 'T' && i[4] === 'V') { // filters out venue vars
+				if (Space.vars[i].min() !== Space.vars[i].max()) { // ignore cases where venues are not determined yet
+					return false;
+				}
+				vArr[Space.vars[i].min()][i[1]]++; // .min() or .max() will give the same output since there's only 1 fixed value
+			} else { // Do nothing
+			}
+		}
+		
+		// Checks that every venue in vArr is never used or used by only 2 teams
+		for (i = 1; i <= numVenues; i++) {// Ignore venue 0 (that's for "bye"s)
+			for (r = 1; r <= numRounds; r++) {
+				if (vArr[i][r] !== 0 && vArr[i][r] !== 2) {
+				return false;
+				}
+			}
+		}
+
+		return true;
+	}
+	
+	function parseSoln(index) {
+		var S = solnArr[index];
+		var S2 = {'O': [], 'V': []};
+		for (var r = 1; r <= numRounds; r++) {
+			for (var t = 1; t <= numTeams; t++) {
+				var varname = varnameArray[r][t];
+				S2.O[varname + 'O'] = S.vars[varname + 'O'].dom[0][0];
+				S2.V[varname + 'V'] = S.vars[varname + 'V'].dom[0][0];
+			}
+		}
+		return S2;
+	}
+	
+	return that;
+}) ();
---
+++ UI.js
@@ -0,0 +1,556 @@
+var worker = new Worker('STS.js');
+worker.addEventListener('message', function(e) {
+	var data = e.data;
+	switch(data.cmd) {
+		case 'soln':
+			updateUI(data.arg);
+			break;
+		case 'partial':
+			partial(data.arg);
+			break;
+		case 'completed':
+			completed(data.arg);
+			break;
+		case 'debug':
+			alert(data.arg);
+			break;
+	}
+}, false);
+
+var teamID = 0;
+var venueID = 0;
+var numTeams = 0;
+var numVenues = 0;
+var numRounds = 0;
+var teamNameArr = [];
+var venueNameArr = [];
+var RGTArr = [];
+var RGVArr = [];
+var state;
+var hlCell = null; // highlighted cell
+
+document.onclick = function(e) {
+	if (hlCell !== null) {
+		removeColours();
+	}
+}
+
+function addTeam() {
+	teamID++;
+	numTeams++;
+	numRounds = numTeams % 2 === 0 ? numTeams - 1: numTeams;
+
+	//Code
+	var name = 'Team ' + teamID;
+	teamNameArr[numTeams] = name;
+	addRGTArr();
+	addRGVArr();
+	//initScheTable();
+
+	//HTML
+	var input = $('<input type="text"/>').attr({
+											id:			'team' + numTeams,
+											value:		name,
+											onchange:	'updateTeamName(' + numTeams + ')'
+										});
+	var del =	$('<a>').attr({
+							class:		'button delete',
+							onclick:	'delTeam(' + numTeams + ')'
+						}).text(' ')
+	$('#team').append(del).append(input);		
+
+	if (numVenues === 0 || numVenues < Math.floor(numTeams / 2)) {
+		addVenue();
+	} else {
+		runSTS();
+	}
+}
+
+function delTeam(num) {
+	numTeams--;
+	//var oldNumRounds = numRounds;
+	numRounds = numTeams % 2 === 0 ? numTeams - 1: numTeams;
+	var index = num - 1;
+
+	//Code
+	teamNameArr.splice(num,  1);
+	delRGTArr(num);
+	//initScheTable();
+	
+	//HTML
+	// remove
+	$('#team a:eq(' + num + ')').remove();
+	$('#team' + num).remove();
+	
+	// update
+	$('#team a').each(function(i) {
+		if (i !== 0) { $(this).attr('onclick', 'delTeam(' + i + ')'); }
+	});
+	$('#team input').each(function(i) {
+		$(this).attr({
+					'id': 'team' + (i + 1),
+					'onchange': 'updateTeamName(' + (i + 1) + ')'
+		});
+	});
+	runSTS();
+}
+
+function addVenue() {
+	numVenues++;
+	venueID++;
+	var name = 'Venue ' + venueID;
+	
+	//HTML
+	var input = $('<input type="text"/>').attr({
+											id:			'venue' + numVenues,
+											value:		name,
+											onchange:	'updateVenueName(' + numVenues + ')'
+										});
+	var del =	$('<a>').attr({
+							class:		'button delete',
+							onclick:	'delVenue(' + numVenues + ')'
+						}).text(' ')
+	$('#venue').append(del).append(input);
+
+	//Code
+	venueNameArr[numVenues] = name;
+	if (numVenues !== 1) { addRGVArr(); }
+	
+	runSTS();
+}
+
+function delVenue(num) {
+	numVenues--;
+	//var oldNumRounds = numRounds;
+	//numRounds = numTeams % 2 === 0 ? numTeams - 1: numTeams;
+	var index = num - 1;
+
+	//Code
+	venueNameArr.splice(num,  1);
+	delRGVArr(num);
+	//initScheTable();
+	
+	//HTML
+	// remove
+	$('#venue a:eq(' + num + ')').remove();
+	$('#venue' + num).remove();
+	
+	// update
+	$('#venue a').each(function(i) {
+		if (i !== 0) { $(this).attr('onclick', 'delVenue(' + i + ')'); }
+	});
+	$('#venue input').each(function(i) {
+		$(this).attr({
+					'id': 'venue' + (i + 1),
+					'onchange': 'updateVenueName(' + (i + 1) + ')'
+		});
+	});
+	
+	runSTS();
+}
+
+function updateUI(S) {
+	initScheTable();
+	
+	for (var type in S) {
+		for (var varname in S[type]) {
+			var cell = document.getElementById([varname]);
+			if (type === 'O') {
+				cell.innerText = teamNameArr[S[type][varname]];
+			} else if (type === 'V') {
+				cell.innerText = venueNameArr[S[type][varname]];
+			}
+		}
+	}
+	initConstraints();
+}
+
+function updateSlider(num) {
+	var slider = document.getElementById('slider');
+	if (num !== 0) {
+		slider.min = 1;
+		slider.max = num;
+		if (num === 1) {
+			//getSpace(1);
+			drag(1);
+		}
+	} else {
+		slider.min = 0;
+		slider.max = 0;
+		slider.value = 0;
+	}
+	var numSoln = document.getElementById('numSoln');
+	numSoln.innerText = num;
+}
+
+function play() {
+	worker.postMessage({'cmd': 'run', 'arg': 10});
+	$('#playpause').attr({
+		class:		'button pause',
+		onclick:	'pause()'
+	});
+}
+
+function pause() {
+	$('#playpause').attr({
+		class:		'button play',
+		onclick:	'play()'
+	});
+}
+
+function partial(num) {
+	updateSlider(num);
+	if ($('#playpause').hasClass('pause')) {
+		worker.postMessage({'cmd': 'run', 'arg': 10});
+	}
+}
+
+function completed(num) {
+	updateSlider(num);
+	$('#playpause').attr('class', 'button complete').removeAttr('onclick');
+}
+
+function showRG(r, t) {	
+	if (hlCell !== null) {
+		removeColours();
+	}
+	var cell = document.getElementById('R' + r + 'T' + t);
+	hlCell = cell;
+	//cell.border = 10;
+	cell.setAttribute('class', 'select');
+	for (var o = 1; o <= numTeams; o++) {
+		var team = document.getElementById('team' + o);		
+		if (o === t) {
+			team.style.backgroundColor = '#808080';
+		} else {
+			if (RGTArr[r][t][o] === 0) { // Red
+				team.style.backgroundColor = '#FF0000';
+			} else if (RGTArr[r][t][o] === 1) { // Green
+				team.style.backgroundColor = '#00FF00';
+			}
+			team.setAttribute('onclick', 'invertRGT(' + r + ', ' + t + ', ' + o + ')');
+		}
+	}
+	for (var v = 1; v <= numVenues; v++) {
+		var venue = document.getElementById('venue' + v);		
+		if (RGVArr[r][t][v] === 0) { // Red
+			venue.style.backgroundColor = '#FF0000';
+		} else if (RGVArr[r][t][v] === 1) { // Green
+			venue.style.backgroundColor = '#00FF00';
+		}
+		venue.setAttribute('onclick', 'invertRGV(' + r + ', ' + t + ', ' + v + ')');
+	}
+	window.event.cancelBubble = true;
+}
+
+function removeColours() {
+	for (var t = 1; t <= numTeams; t++) {
+		var elem = document.getElementById('team' + t);
+		elem.style.backgroundColor = '';
+		elem.onclick = null;
+	}	
+	for (var v = 1; v <= numVenues; v++) {
+		var elem = document.getElementById('venue' + v);
+		elem.style.backgroundColor = '';
+		elem.onclick = null;
+	}
+	//hlCell.border = 1;
+	hlCell.setAttribute('class', '');
+	hlCell = null;
+}
+
+function addRGTArr() {
+	for (var r = 1; r <= numRounds; r++) {
+		if(RGTArr[r] === undefined) { RGTArr[r] = []; }
+		for (var t = 1; t <= numTeams; t++) {
+			if(RGTArr[r][t] === undefined) { RGTArr[r][t] = []; }
+			for (var o = 1; o <= numTeams; o++) {
+				if (RGTArr[r][t][o] === undefined) {
+					if(t === o) { RGTArr[r][t][o] = 0;}
+					else { RGTArr[r][t][o] = 1; }
+				}
+			}
+		}
+	}
+}
+
+function delRGTArr(num) {
+	var oldNumTeams = numTeams + 1;
+	var newRGTArr = [];
+	for (var r = 1; r <= numRounds; r++) {
+		newRGTArr[r] = [];
+		var tCount = 1;
+		for (var t = 1; t <= oldNumTeams; t++) {
+			if (t === num) { continue; }
+			newRGTArr[r][tCount] = [];
+			var oCount = 1;
+			for (var o = 1; o <= oldNumTeams; o++) {
+				if (o === num) { continue; }
+				newRGTArr[r][tCount][oCount] = RGTArr[r][t][o];
+				oCount++;
+			}
+			tCount++;
+		}
+	}
+	RGTArr = newRGTArr;
+}
+
+function addRGVArr() {
+	for (var r = 1; r <= numRounds; r++) {
+	if(RGVArr[r] === undefined) { RGVArr[r] = []; }
+		for (var t = 1; t <= numTeams; t++) {
+			if(RGVArr[r][t] === undefined) { RGVArr[r][t] = []; }
+			for (var v = 1; v <= numVenues; v++) {
+				if (RGVArr[r][t][v] === undefined) {
+					RGVArr[r][t][v] = 1;
+				}
+			}
+		}
+	}
+}
+
+function delRGVArr(num) {
+	var oldNumVenues = numVenues + 1;
+	var newRGVArr = [];
+	for (var r = 1; r <= numRounds; r++) {
+		newRGVArr[r] = [];
+		for (var t = 1; t <= numTeams; t++) {
+			newRGVArr[r][t] = [];
+			var vCount = 1;
+			for (var v = 1; v <= oldNumVenues; v++) {
+				if (v === num) { continue; }
+				newRGVArr[r][t][vCount] = RGVArr[r][t][v];
+				vCount++;
+			}
+		}
+	}
+	RGVArr = newRGVArr;
+}
+
+function initScheTable() {
+	var table = document.getElementById('scheTable');
+	table.innerHTML = '<thead></thread><tbody></tbody>';
+	var headerRow = $('#scheTable >thead').append('<tr>').children();
+	for (var i = 0; i <= numRounds; i++) {
+		headerRow.append('<th>');
+		if (i !== 0) { headerRow.children('th:last').text('Round ' + i); }
+	}
+	
+	table = table.tBodies[0];
+	table.insertRow(-1);
+	
+	for (var t = 1; t <= numTeams; t++) {
+		table.insertRow(-1);
+		for (var r = table.rows[t].cells.length; r <= numRounds; r++) {
+			var cell = table.rows[t].insertCell(-1);
+			if(cell.innerText === '') {
+				if (r === 0 && t === 0) { /* Do nothing */ }				
+				else if (t === 0) {	cell.innerText = 'Round ' + r; }
+				else if (r === 0) {	cell.innerText = teamNameArr[t]; }
+				else {					
+					var varname = 'R' + r + 'T' + t;
+					var table2 = document.createElement('table');
+					table2.id = varname;
+					//table2.border = 1;
+					table2.R = r;
+					table2.T = t;
+					table2.setAttribute('onclick', 'showRG(R, T)');
+					cell.appendChild(table2);
+					var row = table2.insertRow(-1);
+					cell = row.insertCell(-1);
+					cell.style.minWidth = 50;
+					cell.id = varname + 'O';
+					cell = row.insertCell(-1);
+					cell.id = varname + 'V';
+					constraints(r, t);
+				}
+			}
+		}
+	}
+	var testWidth = $('#scheTable').css('width');
+	$('#slider').css('width', testWidth);
+}
+
+function parseRGTArr() {
+	var arr = {};
+	for (var r = 1; r <= numRounds; r++) {
+		for (var t = 1; t <= numTeams; t++) {
+			for (var o = 1; o <=numTeams; o++) {
+				if(RGTArr[r][t][o] === 0) {
+					if(arr['R' + r + 'T' + t + 'O'] === undefined) {
+						arr['R' + r + 'T' + t + 'O'] = [];
+					}
+					arr['R' + r + 'T' + t + 'O'].push(o);
+				}
+			}
+		}
+	}
+	return arr;
+}
+
+function parseRGVArr() {
+	var arr = {};
+	for (var r = 1; r <= numRounds; r++) {
+		for (var t = 1; t <= numTeams; t++) {
+			for (var v = 1; v <=numVenues; v++) {
+				if(RGVArr[r][t][v] === 0) {
+					if(arr['R' + r + 'T' + t + 'V'] === undefined) {
+						arr['R' + r + 'T' + t + 'V'] = [];
+					}
+					arr['R' + r + 'T' + t + 'V'].push(v);
+				}
+			}
+		}
+	}
+	return arr;
+}
+
+function drag(num) {
+	var curSoln = document.getElementById('curSoln');
+	curSoln.value = num
+	worker.postMessage({'cmd': 'grab_results', 'arg': (num - 1)});
+}
+
+function invertRGT(r, t, o) {
+	var table = document.getElementById('R' + r + 'T' + t);
+	var elem = document.getElementById('team' + o);
+	var color = elem.style.backgroundColor;
+	if (color === 'rgb(0, 255, 0)') { // If Green
+		elem.style.backgroundColor = '#FF0000';
+		RGTArr[r][t][o] = 0;
+	} else if (color === 'rgb(255, 0, 0)') { // If Red
+		elem.style.backgroundColor = '#00FF00';
+		RGTArr[r][t][o] = 1;
+	}
+	constraints(r, t);
+	runSTS();
+	window.event.cancelBubble = true;
+}
+
+function invertRGV(r, t, v) {
+	var table = document.getElementById('R' + r + 'T' + t);
+	
+	var elem = document.getElementById('venue' + v);
+	var color = elem.style.backgroundColor;
+	if (color === 'rgb(0, 255, 0)') { // If Green
+		elem.style.backgroundColor = '#FF0000';
+		RGVArr[r][t][v] = 0;
+	} else if (color === 'rgb(255, 0, 0)') { // If Red
+		elem.style.backgroundColor = '#00FF00';
+		RGVArr[r][t][v] = 1;
+	}
+	constraints(r, t);
+	runSTS();
+	window.event.cancelBubble = true;
+}
+
+function constraints(r, t) {
+	var table = document.getElementById('R' + r + 'T' + t);
+	var team = table.rows[0].cells[0];
+	var venue = table.rows[0].cells[1];
+	var text;
+	
+	var tCount = 0;
+	for (var o = 1; o <= numTeams; o++) {
+		if (RGTArr[r][t][o] === 0) {
+			tCount++;
+		}
+	}
+	var vCount = 0;
+	for (var v = 1; v <= numVenues; v++) {
+		if (RGVArr[r][t][v] === 0) {
+			vCount++;
+		}
+	}
+	
+	if (tCount > 1) {
+		if (numTeams === tCount) {
+			team.style.backgroundColor = '#FF0000';
+		} else {
+			team.style.backgroundColor = '#00FFFF';
+		}
+	} else {
+		team.style.backgroundColor = '';
+	}
+	if (numTeams - tCount === 1) {
+		text = team.innerText;
+		if (text !== 'undefined') {
+			team.innerHTML = '<b>' + text + '</b>';
+		}
+	}
+	if (vCount > 0) {
+		if (numVenues === vCount) {
+			venue.style.backgroundColor = '#FF0000';
+		} else {
+			venue.style.backgroundColor = '#00FFFF';
+		}
+	} else {		
+		venue.style.backgroundColor = '';
+	}
+	if (numVenues - vCount === 1) {
+		text = venue.innerText;
+		if (text !== 'undefined') {
+			venue.innerHTML = '<b>' + text + '</b>';
+		}
+	}
+}
+
+function runSTS() {	
+	$('#playpause').attr({
+		class:		'button play',
+		onclick:	'play()'
+	});
+	
+	var table = document.getElementById('scheTable');
+	//table.innerHTML = '';
+	
+	//temp
+	var UI_Obj = {};
+	UI_Obj.numTeams = numTeams;
+	UI_Obj.numVenues = numVenues;
+	UI_Obj.constraintTArr = parseRGTArr();
+	UI_Obj.constraintVArr = parseRGVArr();
+	worker.postMessage({'cmd': 'generate_first_soln',
+						'arg': UI_Obj});
+}
+
+function updateTeamName(t) {
+	teamNameArr[t] = $('#team input:eq('+ (t - 1) + ')').val();
+	var slider = document.getElementById('slider');
+	drag(slider.value);
+}
+
+function updateVenueName(v) {
+	venueNameArr[v] = $('#venue input:eq('+ (v - 1) + ')').val();
+	var slider = document.getElementById('slider');
+	drag(slider.value);
+}
+
+function prev() {
+	var slider = document.getElementById('slider');
+	if(parseInt(slider.value) > parseInt(slider.min)) {
+		drag(--slider.value);
+	}
+}
+
+function next() {
+	var slider = document.getElementById('slider');
+	if(parseInt(slider.value) < parseInt(slider.max)) {
+		drag(++slider.value);
+	}
+}
+
+function initConstraints() {
+	for (var r = 1; r <= numRounds; r++) {
+		for (var t = 1; t <= numTeams; t++) {
+			constraints(r, t);
+		}
+	}
+}
+
+function jump(elem) {
+	var num = elem.value;
+	var slider = document.getElementById('slider');
+	slider.value = num;
+	drag(num);
+}
